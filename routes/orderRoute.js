a4ca4bda4f7d2e52e83cf029cf2597cc3ed05c79
johncarlodelrosariocxp <johncarlodelrosario.cxp@gmail.com>
2025-10-10 13:39:43 +0800

    update

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { addOrder, getOrders, getOrderById, updateOrder } = require("../controllers/orderController");
+const {
+  addOrder,
+  getOrders,
+  getOrderById,
+  updateOrder,
+} = require("../controllers/orderController");
 const { isVerifiedUser } = require("../middlewares/tokenVerification");
 const router = express.Router();
-
 
 router.route("/").post(isVerifiedUser, addOrder);
 router.route("/").get(isVerifiedUser, getOrders);
